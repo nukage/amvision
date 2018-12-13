7ca4a72e18b4281808c3ab77ebbfad44fdd89b19
Tom Myruski <tomhavok@gmail.com>
2018-12-13 15:32:00 -0500

    First Commit

---
+++ js/animatelogo.js
@@ -0,0 +1,5 @@
+jQuery(document).ready( function($) {
+
+   $(".site-logo").append('<div class="headerheart"><img src="/wp-content/uploads/2017/10/iheartproducers2.png"  class="pulse animated infinite "></div>');
+
+});
